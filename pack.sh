65b972d71acd52a1931ec68648713801fbb817a3
dong031001 <30747164+dong031001@users.noreply.github.com>
2020-06-21 17:26:57 +0800

    脚本测试3

--- pack.sh
+++ pack.sh
@@ -2,13 +2,13 @@
 PACK_NAME=EnigTech2
 MAJOR_VERSION=1
 MINOR_VERSION=4
-ITERATION_VERSION=2
+ITERATION_VERSION=1
 
 fileName="$PACK_NAME-$MAJOR_VERSION.$MINOR_VERSION.$ITERATION_VERSION-dev.zip"
 
 mv "./$PACK_NAME/minecraft" "./$PACK_NAME/src" 
 
-zip -r "$fileName" "./$PACK_NAME/*"
+zip -r "$fileName" "./$PACK_NAME/src" "./$PACK_NAME/instance.cfg" "./$PACK_NAME/logo.png" "./$PACK_NAME/manifest.json"
 
 mkdir artifacts
 
