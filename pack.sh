58e7d2ee599a1646a066a0ac356a408b918d97ed
dong031001 <30747164+dong031001@users.noreply.github.com>
2020-06-21 17:17:55 +0800

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -8,7 +8,7 @@ fileName="$PACK_NAME-$MAJOR_VERSION.$MINOR_VERSION.$ITERATION_VERSION-dev.zip"
 
 mv "./$PACK_NAME/minecraft" "./$PACK_NAME/src" 
 
-zip -r "$fileName" "./$PACK_NAME"
+zip -r "$fileName" "./$PACK_NAME/*"
 
 mkdir artifacts
 
